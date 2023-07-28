0bcdad8c0b16d7ca92ea8ad50f48eea9324efca2
bestradag05 <bestradag05@gmail.com>
2023-07-28 17:13:18 -0500

    Alertas y fondo para juego

--- js/app.js
+++ js/app.js
@@ -16,18 +16,53 @@ function iniciarJuego() {
 
 function reiniciarJuego()
 {
-    finalizarJuego();
-    mostrarTopo();
-    intervalorTopo = setInterval(mostrarTopo, 2000);
-    intervalorTiempo = setInterval(actualizarTiempo, 1000);
-    document.getElementById('tiempo').innerHTML = 15;
-    document.getElementById("puntaje").innerHTML = 0;
-    puntos = 0;
-    valorTiempo = 15;
+    Swal.fire({
+        title: '¡ Bien Echo !',
+        html: `<b>Puntos: ${puntos}</b><br><br>
+        <p>¿ Deseas reiniciar el juego ?</p>`,
+        icon: `warning`,
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, reiniciar!',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Juego Reiniciado!',
+            'Consigue tu mejor Score !',
+            'success'
+          ).then((result) => {
+                clearInterval(intervalorTopo);
+                clearInterval(intervalorTiempo);
+            
+            if (result.isConfirmed) {
+                
+                mostrarTopo();
+                intervalorTopo = setInterval(mostrarTopo, 2000);
+                intervalorTiempo = setInterval(actualizarTiempo, 1000);
+    
+                document.getElementById('tiempo').innerHTML = 15;
+                document.getElementById("puntaje").innerHTML = 0;
+                puntos = 0;
+                valorTiempo = 15;
+            }
+            });
+          
+        }
+      })
+
 }
 
 
 function finalizarJuego(){
+    Swal.fire({
+        title: '¡ Bien Echo !',
+        html: `<b>Puntos: ${puntos}</b><br><br>
+        <p>El juego finalizo, si deseas volver a jugar cierra esta ventanda y <b>¡ Diviertete !</b></p>`,
+        icon: `success`,
+      });
+
     clearInterval(intervalorTopo);
     clearInterval(intervalorTiempo);
 }
@@ -64,5 +99,3 @@ function actualizarTiempo() {
         
     }
 }
-
-
